997a6ec61794e7594dad806299bce74494181c67
Klim Androsov <kfasf@list.ru>
2025-11-12 16:17:03 +0400

    chore: add Husky pre-commit hook

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert'],
+    ],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+  },
+};
