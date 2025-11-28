bce7de0bd51c302b4417e70282516eb9a3794cdf
Klim Androsov <kfasf@list.ru>
2025-11-28 12:41:10 +0400

    chore: integrated prisma and changed deploy files

--- apps/lumio/src/app.module.ts
+++ apps/lumio/src/app.module.ts
@@ -2,11 +2,13 @@ import { configModule } from '../../../libs/core/config-dynamic.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from '../../../prisma/prisma.service';
 
 @Module({
   imports: [configModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {
   static forRoot: any;
