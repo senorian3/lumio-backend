2cd41c9c9791f474acc11f64d93731f3814aead4
Klim Androsov <kfasf@list.ru>
2025-11-28 23:42:33 +0400

    chore: fixed scripts with env files, dockerfiles, prisma service import

--- libs/core/config-dynamic.module.ts
+++ libs/core/config-dynamic.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 export const configModule = ConfigModule.forRoot({
   envFilePath: [
     process.env.ENV_FILE_PATH?.trim() || '',
-    `.env.${process.env.NODE_ENV}.local`,
     `.env.${process.env.NODE_ENV}`,
     '.env.production',
   ],
