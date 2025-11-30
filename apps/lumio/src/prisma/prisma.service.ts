aa24a42197256f82ad584afd744f0aec779531f7
Ilya Kozlovski <kozlvskiilya@gmail.com>
2025-11-30 10:06:50 +0300

    chore(prisma): add test database scripts

--- apps/lumio/src/prisma/prisma.service.ts
+++ apps/lumio/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma-lumio/client';
+import { PrismaClient } from 'generated/prisma-lumio';
 
 @Injectable()
 export class PrismaService
