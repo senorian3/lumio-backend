cf96082adcdb042ab3f0b69353866efca559a448
Klim Androsov <kfasf@list.ru>
2025-12-11 23:50:06 +0400

    refactor(sessions): move mapping from query repo to get all sessions usecase

--- apps/lumio/src/modules/sessions/domain/infrastructure/session.query.repository.ts
+++ apps/lumio/src/modules/sessions/domain/infrastructure/session.query.repository.ts
@@ -1,23 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@lumio/prisma/prisma.service';
-import { OutputSessionType } from '../../api/dto/output/output';
 import { SessionEntity } from '../session.entity';
 @Injectable()
 export class QuerySessionsRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  async getAllSessions(userId: number): Promise<OutputSessionType[]> {
+  async getAllSessions(userId: number): Promise<SessionEntity[]> {
     const allSessions: SessionEntity[] = await this.prisma.session.findMany({
       where: { user: { id: userId }, deletedAt: null },
     });
 
-    return allSessions.map(
-      (session) =>
-        new OutputSessionType(
-          session.deviceName,
-          session.ip,
-          session.createdAt.toISOString(),
-        ),
-    );
+    return allSessions;
   }
 }
