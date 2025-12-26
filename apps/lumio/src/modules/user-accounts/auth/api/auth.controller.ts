5d5d38c8827932f2a1c8c1e681d96e356149d533
Klim Androsov <kfasf@list.ru>
2025-12-26 20:23:29 +0400

    fix(samesite): to none

--- apps/lumio/src/modules/user-accounts/auth/api/auth.controller.ts
+++ apps/lumio/src/modules/user-accounts/auth/api/auth.controller.ts
@@ -154,7 +154,7 @@ export class AuthController {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: false,
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: '/',
     });
