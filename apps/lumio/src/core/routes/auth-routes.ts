49dde3906d85746f927cbee815e5fb072bfea017
Klim Androsov <kfasf@list.ru>
2026-01-10 23:40:06 +0400

    refactor: change routes arch

--- apps/lumio/src/core/routes/auth-routes.ts
+++ apps/lumio/src/core/routes/auth-routes.ts
@@ -11,14 +11,4 @@ export const AUTH_ROUTES = {
   YANDEX_CALLBACK: 'yandex/callback',
   REFRESH_TOKEN: 'refresh-token',
   ME: 'me',
-} as const;
-
-export const SECURITY_BASE = 'security/devices';
-
-export const POSTS_BASE = 'posts';
-
-export const POSTS_ROUTES = {
-  GET_MY_POSTS: 'my',
-} as const;
-
-export const PROFILE_BASE = 'profile';
+};
---
+++ apps/lumio/src/core/routes/post-routes.ts
@@ -0,0 +1,5 @@
+export const POST_BASE = 'posts';
+
+export const POST_ROUTES = {
+  GET_MY_POSTS: 'my',
+};
