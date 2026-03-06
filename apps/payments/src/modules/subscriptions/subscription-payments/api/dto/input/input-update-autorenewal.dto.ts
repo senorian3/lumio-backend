aeade5c39d63752747c0d663778aa1c57ab69646
Klim Androsov <kfasf@list.ru>
2026-03-07 03:05:51 +0400

    fix(payments): add validation decorators to subscription DTOs

--- apps/payments/src/modules/subscriptions/subscription-payments/api/dto/input/input-update-autorenewal.dto.ts
+++ apps/payments/src/modules/subscriptions/subscription-payments/api/dto/input/input-update-autorenewal.dto.ts
@@ -1,5 +1,20 @@
+import {
+  IsNumberString,
+  IsNotEmpty,
+  IsBoolean,
+  IsString,
+} from 'class-validator';
+
 export class InputChangeAutorenewalSubscriptionPaymentDto {
+  @IsNumberString()
+  @IsNotEmpty()
   profileId: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
   autoRenewal: boolean;
+
+  @IsString()
+  @IsNotEmpty()
   subscriptionId: string;
 }
