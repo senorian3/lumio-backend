a27b680f154d6b60b482b0048c110adef9f02d45
Klim Androsov <kfasf@list.ru>
2026-03-13 00:34:28 +0400

    fix(scheduler): fix double trigger scheduler for app

--- apps/lumio/src/modules/notifications/notifications.module.ts
+++ apps/lumio/src/modules/notifications/notifications.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 import { NotificationsGateway } from './application/notifications.gateway';
 import { NotificationRepository } from '@lumio/modules/notifications/domain/infrastructure/notification.repository';
 import { NotificationsService } from './application/notifications.service';
@@ -11,7 +10,7 @@ const repositories = [NotificationRepository, NotificationQueryRepository];
 const services = [NotificationsService, NotificationsScheduler];
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [],
   providers: [NotificationsGateway, ...repositories, ...services],
   exports: [NotificationsService],
 })
